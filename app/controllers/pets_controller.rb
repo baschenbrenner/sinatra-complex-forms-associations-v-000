770b92143eef4d70799eca4e88db0cd188e5304f
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-21 11:03:57 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -45,7 +45,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id/edit' do
-    binding.pry
+    
     @pet=Pet.find(params[:id])
     @pet.update(name: params[:pet]["name"])
     @pet.owner = Owner.find(params[:owner_id])
